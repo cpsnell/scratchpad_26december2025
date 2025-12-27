002f5b76a09890861b43d3e1a6f45b142cf55ddd
cpsnell <cpsnell@gmail.com>
2025-12-26 21:02:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+with open("file1.txt") as file1:
+  list1 = file1.readlines()
+    
+with open("file2.txt") as file2:
+  list2 = file2.readlines()
+    
+result = [int(num) for num in list1 if num in list2]
+ 
+print(result)
 
+
+'''
 import csv
 
 with open("weather_data.csv") as data_file:
@@ -21,3 +32,4 @@ with open("weather_data.csv", mode="w", encoding="utf-8") as file:
 
 '''new_list = [new_item for item in list]'''
 '''example: new_list = [n + 1 for n in numbers]
+'''
